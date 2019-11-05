b98d7602e9037b83605d8b9e7524465bd2e4c59e
griffin sharp <gsharpdev@gmail.com>
2019-11-05 09:31:41 -0800

    Add sessions reducer and users reducer, start on entities reducer to track relational data

---
+++ frontend/reducers/entities_reducer.js
@@ -0,0 +1,8 @@
+import { combineReducers } from 'redux';
+import usersReduxer from './users_reducer';
+
+
+// Keeps track of all of our relational data for Kicker.
+// Combine our reducers that handle relational data
+
+export default 
--- frontend/reducers/session_reducer.js
+++ frontend/reducers/session_reducer.js
@@ -3,22 +3,21 @@ import { RECEIVE_CURRENT_USER, LOGOUT_CURRENT_USER } from '../actions/session_ac
 // RECEIVE_CURRENT_USER - sets id to the action's user.id
 // LOGOUT_CURRENT_USER - sets id to null
 
-const _nullUser = Object.freeze({
+const _nullSession = Object.freeze({
     id: null
 });
 
 // Reducers need to take in a state and action and return a new state.
 // Freezing state prevents our oldstate from ever being accidently modified/mutated.
 // Setup a new state object, essentially copying oldState so we can return a modified version as the newState.
-const sessionReducer = (oldState = _nullUser, action) => {
+const sessionReducer = (oldState = _nullSession, action) => {
     Object.freeze(oldState);
-    const newState = Object.assign({}, oldState);
     switch (action.type) {
         case RECEIVE_CURRENT_USER:
-            newState[action.currentUser.id] = action.currentUser;
-            return newState;
+            const id = action.currentUser.id;
+            return id;
         case LOGOUT_CURRENT_USER:
-            return _nullUser;
+            return null;
         default:
             return oldState;
     }
