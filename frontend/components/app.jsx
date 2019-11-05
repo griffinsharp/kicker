d8dd30a073014151206a09228029790c7788bd79
griffin sharp <gsharpdev@gmail.com>
2019-11-05 11:19:16 -0800

    Add root and app component and modified entry file

---
+++ frontend/components/app.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+// Purely functional component
+
+const App = () => (
+    <div>
+        <h1>Kicker!</h1>
+    </div>
+);
+
+export default App
--- frontend/kicker.jsx
+++ frontend/kicker.jsx
@@ -1,21 +1,22 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import {login, logout, signup} from './util/session_api_util';
+import Root from './components/root';
 import configureStore from './store/store';
-import {receiveCurrentUser}from "./actions/session_actions"
+// import {login, logout, signup} from './util/session_api_util';
+// import {receiveCurrentUser}from "./actions/session_actions"
 
 document.addEventListener('DOMContentLoaded', () => {
     const root = document.getElementById('root');
-    // const store = configureStore();
-    ReactDOM.render(<h1>Welcome To Kicker!</h1>, root);
+    const store = configureStore();
+    ReactDOM.render(<Root store={store}/>, root);
 
     // *** BELOW IS FOR TESTING ONLY, REMOVE ONCE PUSHED TO PRODUCTION ***
-    window.login = login;
-    window.logout = logout;
-    window.signup = signup;
-    const store = configureStore();
-    window.receiveCurrentUser = receiveCurrentUser;
-    window.getState = store.getState;
-    window.dispatch = store.dispatch;
+    // window.login = login;
+    // window.logout = logout;
+    // window.signup = signup;
+    // const store = configureStore();
+    // window.receiveCurrentUser = receiveCurrentUser;
+    // window.getState = store.getState;
+    // window.dispatch = store.dispatch;
 })
 
