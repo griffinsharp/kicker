da7bd80d843a8a58123d98153148009d571f10f5
griffin sharp <gsharpdev@gmail.com>
2019-11-13 22:30:45 -0800

    pre-heroku

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -19,7 +19,7 @@ Rails.application.configure do
   # config.require_master_key = true
 
   # Disable serving static files from the `/public` folder by default since
-  # Apache or NGINX already handles this.
+  # Apache or NGINX already handles this.y
   config.public_file_server.enabled = ENV['RAILS_SERVE_STATIC_FILES'].present?
 
   # Compress JavaScripts and CSS.
