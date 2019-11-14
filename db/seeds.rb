d21eae6f12c524fc46292b0537f78c8fac1e15da
griffin sharp <gsharpdev@gmail.com>
2019-11-14 15:57:15 -0800

    heroku

--- db/seeds.rb
+++ db/seeds.rb
@@ -169,7 +169,7 @@ shoeCreatorProject.save!
 
 reward0KickIt = Reward.create!(amount: 10, desc: "test test test test", subdesc: "test test test test test", delivery: "Tomorrow", shipping: "International", num_backers: 48, project_id: kickItProject.id)  
 reward1KickIt = Reward.create!(amount: 20, desc: "test test test", subdesc: "test test test", delivery: "Tomorrow", shipping: "International", num_backers: 45, project_id: kickItProject.id) 
-reward2KickIt = Reward.create!(amount: 50, desc: "test test test test", subdesc: "test test test", delivery: "Tomorrow", shipping: "International", num_backers: 90, project_id: kickItProject.id
+reward2KickIt = Reward.create!(amount: 50, desc: "test test test test", subdesc: "test test test", delivery: "Tomorrow", shipping: "International", num_backers: 90, project_id: kickItProject.id)
 
 reward0AllBirds = Reward.create!(amount: 10, desc: "test test test test", subdesc: "test test test test test", delivery: "Tomorrow", shipping: "International", num_backers: 48, project_id: allBirdsProject.id)  
 reward1AllBirds = Reward.create!(amount: 20, desc: "test test test", subdesc: "test test test", delivery: "Tomorrow", shipping: "International", num_backers: 45, project_id: allBirdsProject.id) 
