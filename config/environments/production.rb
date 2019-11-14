617f8c8977a70b2a949aec983489f353fb866774
griffin sharp <gsharpdev@gmail.com>
2019-11-13 22:47:24 -0800

    heroku

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -78,6 +78,8 @@ Rails.application.configure do
 
   # Use default logging formatter so that PID and timestamp are not suppressed.
   config.log_formatter = ::Logger::Formatter.new
+  
+  config.assets.compile = true
 
   # Use a different logger for distributed setups.
   # require 'syslog/logger'
