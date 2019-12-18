5c2462864b730ea034cf637d7833dc1b56d09ed0
griffin sharp <gsharpdev@gmail.com>
2019-12-18 11:27:34 -0800

    Add scroll to top component and wrap app below router

--- frontend/components/root.jsx
+++ frontend/components/root.jsx
@@ -2,14 +2,17 @@ import React from 'react';
 import { Provider } from 'react-redux';
 import { HashRouter } from 'react-router-dom';
 import App from './app';
+import ScrollToTop from './scroll';
 
 // App will be rendered for all routes in Kicker. We will define our routes in the app component.
 // Provider will pass down the store to the rest of our components for us. 
 
 const Root = ({ store }) => (
     <Provider store={store}>
-        <HashRouter>
-            <App />
+        <HashRouter >
+            <ScrollToTop>
+                <App /> 
+            </ScrollToTop>
         </HashRouter>
     </Provider>
 );
