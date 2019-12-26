7e2905fb70f992dc799122a44240862c52fe2aa8
griffin sharp <gsharpdev@gmail.com>
2019-12-25 22:50:57 -0800

    Style modal, need scroll overflow

--- frontend/components/navbar/navbar.jsx
+++ frontend/components/navbar/navbar.jsx
@@ -3,13 +3,15 @@ import { Link } from 'react-router-dom';
 import Logo from '../../../app/assets/images/logo.png';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faSearch } from '@fortawesome/free-solid-svg-icons';
+import { faTimes } from '@fortawesome/free-solid-svg-icons';
 
 class Navbar extends React.Component {
 
     constructor(props) {
         super(props);
         this.state = {
-          modalAppear: "hidden"
+          modalAppear: "hidden",
+          modalAppearClear: "hidden"
         };
 
         this.handleExplore = this.handleExplore.bind(this);
@@ -18,8 +20,10 @@ class Navbar extends React.Component {
   handleExplore() {
     if (this.state.modalAppear === "hidden") {
       this.setState({ modalAppear: "modal" });
+      this.setState({ modalAppearClear: "modalclear" });
     } else if (this.state.modalAppear === "modal") {
       this.setState({ modalAppear: "hidden" });
+      this.setState({ modalAppearClear: "hidden" });
     }
 
   }
@@ -50,48 +54,194 @@ class Navbar extends React.Component {
 
         return (
           <div className="navbar-and-modal">
-              <div className={this.state.modalAppear}>
-              <div className="modal-x-btn" onClick={() => this.handleExplore()}>X</div>
-                <div className="modal-header">Collections
+            <div className={this.state.modalAppear}>
+              <div className={this.state.modalAppearClear}>
+              <div className="modal-wrapper">
+                <div className="modal-top">
+                  <div className="modal-header">Collections</div>
+                  
+                  <div className="modal-x-btn" onClick={() => this.handleExplore()}><FontAwesomeIcon className="times-icon" icon={faTimes} /></div>
+                </div>
                   <div className="modal-links">
-                  <Link onClick={() => this.handleExplore()} to={{
+                  <Link className="modal-link" onClick={() => this.handleExplore()} to={{
                       pathname: '/search',
                     state: {
                       category_id: '', location: '', filter: 'loved', filtprojects: ''
                     }
                       }}> Projects We Love </Link>
-                  <Link onClick={() => this.handleExplore()} to={{
+                  <Link className="modal-link" onClick={() => this.handleExplore()} to={{
                     pathname: '/search',
                     state: {
                       category_id: '', location: '', filter: 'funded', filtprojects: ''
                     }
                   }}> Nearly Funded </Link>
-                  <Link onClick={() => this.handleExplore()} to={{
+                  <Link className="modal-link" onClick={() => this.handleExplore()} to={{
                     pathname: '/search',
                     state: {
                       category_id: '', location: '', filter: 'newest', filtprojects: ''
                     }
                   }}>Just Launched </Link>
-                  <Link onClick={() => this.handleExplore()} to={{
+                  <Link className="modal-link" onClick={() => this.handleExplore()} to={{
                     pathname: '/search',
                     state: {
                       category_id: '', location: '', filter: '', filtprojects: ''
                     }
                   }}>Everything </Link>
                   </div>
+                
+                <div className="modal-top">
+                  <div className="modal-header">Categories</div>
                 </div>
-              <div className="modal-header">Categories
                   <div className="modal-links">
-                  <Link onClick={() => this.handleExplore()} className="" to="/category/1">Arts</Link>
-                  <Link onClick={() => this.handleExplore()} className="" to="/category/2">Comics & Illustration</Link>
-                  <Link onClick={() => this.handleExplore()} className="" to="/category/3">Design & Tech</Link>
-                  <Link onClick={() => this.handleExplore()} className="" to="/category/4">Film</Link>
-                  <Link onClick={() => this.handleExplore()} className="" to="/category/5">Food & Craft</Link>
-                  <Link onClick={() => this.handleExplore()} className="" to="/category/6">Games</Link>
-                  <Link onClick={() => this.handleExplore()} className="" to="/category/7">Music</Link>
-                  <Link onClick={() => this.handleExplore()} className="" to="/category/8">Publishing</Link>
+                  <Link onClick={() => this.handleExplore()} className="modal-link" to="/category/1">Arts</Link>
+                  <Link onClick={() => this.handleExplore()} className="modal-link" to="/category/2">Comics & Illustration</Link>
+                  <Link onClick={() => this.handleExplore()} className="modal-link" to="/category/3">Design & Tech</Link>
+                  <Link onClick={() => this.handleExplore()} className="modal-link" to="/category/4">Film</Link>
+                  <Link onClick={() => this.handleExplore()} className="modal-link" to="/category/5">Food & Craft</Link>
+                  <Link onClick={() => this.handleExplore()} className="modal-link" to="/category/6">Games</Link>
+                  <Link onClick={() => this.handleExplore()} className="modal-link" to="/category/7">Music</Link>
+                  <Link onClick={() => this.handleExplore()} className="modal-link" to="/category/8">Publishing</Link>
                 </div>
-              </div>
+                  <div className="modal-top">
+                    <div className="modal-header">Locations</div>
+                  </div>
+                  <div className="modal-links">
+                    <Link className="modal-link" onClick={() => this.handleExplore()} to={{
+                      pathname: '/search',
+                      state: {
+                        category_id: '', location: 'Earth', filter: '', filtprojects: ''
+                      }
+                    }}>Earth</Link>
+                  <Link className="modal-link" onClick={() => this.handleExplore()} to={{
+                    pathname: '/search',
+                    state: {
+                      category_id: '', location: 'Australia', filter: '', filtprojects: ''
+                    }
+                  }}>Australia</Link>
+                  <Link className="modal-link" onClick={() => this.handleExplore()} to={{
+                    pathname: '/search',
+                    state: {
+                      category_id: '', location: 'Austria', filter: '', filtprojects: ''
+                    }
+                  }}>Austria</Link>
+                  <Link className="modal-link" onClick={() => this.handleExplore()} to={{
+                    pathname: '/search',
+                    state: {
+                      category_id: '', location: 'Belgium', filter: '', filtprojects: ''
+                    }
+                  }}>Belgium</Link>
+                  <Link className="modal-link" onClick={() => this.handleExplore()} to={{
+                    pathname: '/search',
+                    state: {
+                      category_id: '', location: 'Canada', filter: '', filtprojects: ''
+                    }
+                  }}>Canada</Link>
+                  <Link className="modal-link" onClick={() => this.handleExplore()} to={{
+                    pathname: '/search',
+                    state: {
+                      category_id: '', location: 'Denmark', filter: '', filtprojects: ''
+                    }
+                  }}>Denmark</Link>
+                  <Link className="modal-link" onClick={() => this.handleExplore()} to={{
+                    pathname: '/search',
+                    state: {
+                      category_id: '', location: 'France', filter: '', filtprojects: ''
+                    }
+                  }}>France</Link>
+                  <Link className="modal-link" onClick={() => this.handleExplore()} to={{
+                    pathname: '/search',
+                    state: {
+                      category_id: '', location: 'Germany', filter: '', filtprojects: ''
+                    }
+                  }}>Germany</Link>
+                                    <Link className="modal-link" onClick={() => this.handleExplore()} to={{
+                    pathname: '/search',
+                    state: {
+                      category_id: '', location: 'Ireland', filter: '', filtprojects: ''
+                    }
+                  }}>Ireland</Link>
+                                    <Link className="modal-link" onClick={() => this.handleExplore()} to={{
+                    pathname: '/search',
+                    state: {
+                      category_id: '', location: 'Italy', filter: '', filtprojects: ''
+                    }
+                  }}>Italy</Link>
+                                    <Link className="modal-link" onClick={() => this.handleExplore()} to={{
+                    pathname: '/search',
+                    state: {
+                      category_id: '', location: 'Japan', filter: '', filtprojects: ''
+                    }
+                  }}>Japan</Link>
+                                    <Link className="modal-link" onClick={() => this.handleExplore()} to={{
+                    pathname: '/search',
+                    state: {
+                      category_id: '', location: 'Luxembourg', filter: '', filtprojects: ''
+                    }
+                  }}>Luxembourg</Link>
+                                    <Link className="modal-link" onClick={() => this.handleExplore()} to={{
+                    pathname: '/search',
+                    state: {
+                      category_id: '', location: 'Mexico', filter: '', filtprojects: ''
+                    }
+                  }}>Mexico</Link>
+                                    <Link className="modal-link" onClick={() => this.handleExplore()} to={{
+                    pathname: '/search',
+                    state: {
+                      category_id: '', location: 'New Zealand', filter: '', filtprojects: ''
+                    }
+                  }}>New Zealand</Link>
+                  <Link className="modal-link" onClick={() => this.handleExplore()} to={{
+                    pathname: '/search',
+                    state: {
+                      category_id: '', location: 'Norway', filter: '', filtprojects: ''
+                    }
+                  }}>Norway</Link>
+                  <Link className="modal-link" onClick={() => this.handleExplore()} to={{
+                    pathname: '/search',
+                    state: {
+                      category_id: '', location: 'Singapore', filter: '', filtprojects: ''
+                    }
+                  }}>Singapore</Link>
+                  <Link className="modal-link" onClick={() => this.handleExplore()} to={{
+                    pathname: '/search',
+                    state: {
+                      category_id: '', location: 'Spain', filter: '', filtprojects: ''
+                    }
+                  }}>Spain</Link>
+                  <Link className="modal-link" onClick={() => this.handleExplore()} to={{
+                    pathname: '/search',
+                    state: {
+                      category_id: '', location: 'Sweden', filter: '', filtprojects: ''
+                    }
+                  }}>Sweden</Link>
+                  <Link className="modal-link" onClick={() => this.handleExplore()} to={{
+                    pathname: '/search',
+                    state: {
+                      category_id: '', location: 'Switzerland', filter: '', filtprojects: ''
+                    }
+                  }}>Switzerland</Link>
+                  <Link className="modal-link" onClick={() => this.handleExplore()} to={{
+                    pathname: '/search',
+                    state: {
+                      category_id: '', location: 'the Netherlands', filter: '', filtprojects: ''
+                    }
+                  }}>the Netherlands</Link>
+                  <Link className="modal-link" onClick={() => this.handleExplore()} to={{
+                    pathname: '/search',
+                    state: {
+                      category_id: '', location: 'the United Kingdom', filter: '', filtprojects: ''
+                    }
+                  }}>the United Kingdom</Link>
+                  <Link className="modal-link" onClick={() => this.handleExplore()} to={{
+                    pathname: '/search',
+                    state: {
+                      category_id: '', location: 'the United States', filter: '', filtprojects: ''
+                    }
+                  }}>the United States</Link>
+                
+                  </div>
+                </div>
+                </div>
               </div>
             <div className="navbar">
               <div className="left-bar">
