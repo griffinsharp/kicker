1f8255a8edad734c073e6932e4af91357d06baa0
griffin sharp <gsharpdev@gmail.com>
2019-12-26 15:46:37 -0800

    Start style categories

--- frontend/components/categorybar/categorybar.jsx
+++ frontend/components/categorybar/categorybar.jsx
@@ -5,25 +5,38 @@ class CategoryBar extends React.Component {
   render() {
     return (
       <div className="category-list">
+        <div className="cat-item-and-line">
           <Link className="cat-list-item" to="/category/1">Arts</Link>
-        
+          <div className="cat-list-line"></div>
+        </div>
+        <div className="cat-item-and-line">
           <Link className="cat-list-item" to="/category/2">Comics & Illustration</Link>
-        
+          <div className="cat-list-line"></div>
+        </div>
+        <div className="cat-item-and-line">
           <Link className="cat-list-item" to="/category/3">Design & Tech</Link>
-        
+          <div className="cat-list-line"></div>
+        </div>
+        <div className="cat-item-and-line">
           <Link className="cat-list-item" to="/category/4">Film</Link>
-        
-
+          <div className="cat-list-line"></div>
+        </div>
+        <div className="cat-item-and-line">
           <Link className="cat-list-item" to="/category/5">Food & Craft</Link>
-        
-
+          <div className="cat-list-line"></div>
+        </div>
+        <div className="cat-item-and-line">
           <Link className="cat-list-item" to="/category/6">Games</Link>
-        
-
+          <div className="cat-list-line"></div>
+        </div>
+        <div className="cat-item-and-line">
           <Link className="cat-list-item" to="/category/7">Music</Link>
-        
-
+          <div className="cat-list-line"></div>
+        </div>
+        <div className="cat-item-and-line">
           <Link className="cat-list-item" to="/category/8">Publishing</Link>
+          <div className="cat-list-line"></div>
+        </div>
         
       </div>
     );
